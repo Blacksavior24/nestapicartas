7b8a8f1dcf8254ba8cc7a670c1f62438b09f52e8
Emerson Villalta <reaperfocs@gmail.com>
2025-04-16 13:49:43 -0500

    path logger sever

--- src/fileupload/fileupload.service.ts
+++ src/fileupload/fileupload.service.ts
@@ -1,16 +1,17 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { CreateFileuploadDto } from './dto/create-fileupload.dto';
 import { join } from 'path';
 import { existsSync } from 'fs';
 
 @Injectable()
 export class FileuploadService {
-  
+  private readonly logger = new Logger(FileuploadService.name)
   getStaticProductImage ( imageName: string ){
 
     const path = join( __dirname, '../../files/', imageName);
 
     console.log("path finaly", path)
+    this.logger.log(`path finaly ${path}`)
     if (!existsSync(path)) {
       throw new BadRequestException(`No existe el archivo`)
     }
